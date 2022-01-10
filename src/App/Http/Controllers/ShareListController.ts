79803b494c592de8fe6888e4bac7f5664aba3683
Ricardo Rocha <me@rrocha.uk>
2022-01-10 12:17:48 +0000

    * ShareableList logic;

--- src/App/Http/Controllers/ShareListController.ts
+++ src/App/Http/Controllers/ShareListController.ts
@@ -1,11 +1,25 @@
 import { get, Controller, controller, request, response } from '@envuso/core/Routing';
+import { List } from '../../Models/List';
+import { Item } from '../../Models/Item';
 
 @controller('/share')
 export class ShareListController extends Controller {
   @get('/l/:id')
   async shareList() {
     const { id } = request().params().all();
+    const list = await List.query().where({
+      _id: id,
+      isPrivate: false,
+    }).get();
 
-    return response().json({ message: 'list to share yay' }, 200);
+    if (!list[0]) {
+      return response().json({ items: [], message: `couldn't find list` }, 200);
+    }
+
+    const items = await Item.query().where({
+      list: id
+    }).get();
+
+    return response().json({ items, message: 'list to share yay' }, 200);
   }
 }
