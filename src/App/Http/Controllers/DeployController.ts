733b0767bf5cc4e32c56ba03b18e0d17f36e5aae
Ricardo Rocha <me@rrocha.uk>
2022-06-25 21:11:41 +0100

    * build process for notyet fe;

--- src/App/Http/Controllers/DeployController.ts
+++ src/App/Http/Controllers/DeployController.ts
@@ -1,18 +1,27 @@
-import { Controller, controller, post, request } from '@envuso/core/Routing'
+import * as path from 'path';
+import { promisify } from 'util';
+import { exec } from 'child_process';
+import { Controller, controller, post, request, response } from '@envuso/core/Routing';
 
 @controller('/deploy')
 export class DeployController extends Controller {
   @post('/frontend')
-  deployFrontend() {
+  async deployFrontend() {
+    const { action, pull_request } = request().body();
 
-    const { action } = request().body();
-
-    if (!action) {
+    if (action !== 'closed' || !pull_request) {
       return;
     }
 
-    console.log(action);
+    if (pull_request.base && pull_request.base.ref !== 'main') {
+      return;
+    }
 
-    return;
+    // run frontend script
+    const script = path.resolve(process.cwd(), '../frontend.sh');
+    const buildProcess = await promisify(exec);
+    await buildProcess(`sh ${script}`);
+
+    return response().json({ done: 'build all done and deployed...' }, 200);
   }
 }
